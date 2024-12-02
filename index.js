fb4a0b6e3639891795eaaec787ded2cc0edc183d
SunshineAlis <bumaam@yahoo.com>
2024-12-02 17:18:24 +0800

    firstvariable

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("hello from Js Variables");
+let age = 15;
+console.log(age);
+age = 40;
+console.log(age);
+const myName = "Bat";
+console.log(myName);
+const mySonName = "Elbeg";
+console.log(mySonName);
+console.log("true or false");
+const isMarried = false;
+console.log(age);
+let number = 32;
+const mybrothername = "Bayraa";
+console.log(mybrothername);
